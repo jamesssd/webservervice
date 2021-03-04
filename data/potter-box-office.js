a6eb031e51a62b2d3b3f9a6724d8e407d1c80411
James Dizon <dizonjerlin@gmail.com>
2021-03-04 09:03:53 -0800

    updated files

---
+++ data/potter-box-office.js
@@ -0,0 +1,104 @@
+{
+	"title": "Harry Potter Films by Box Office Totals",
+	
+	"films":[
+             {
+				"Film":1,
+				"Title":"Harry Potter and the Sorcerer's Stone",
+				"Year":2001,
+				"Director":"Chris Columbus",
+				"Producer":"David Heyman",
+				"Screenwriter": "Steve Kloves",
+				"Composer":"John Williams",
+				"Budget":"$125,000,000.00",
+				"BoxOffice":"$978,755,371",
+				"Image":"hp1.jpg"
+			  },
+              {
+				"Film":2,
+				"Title":"Harry Potter and the Chamber of Secrets",
+				"Year":2002,
+				"Director":"Chris Columbus",
+				"Producer":"David Heyman",
+				"Screenwriter": "Steve Kloves",
+				"Composer":"John Williams",
+				"Budget":"$100,000,000.00",
+				"BoxOffice":"$878,979,634",
+				"Image":"hp2.jpg"
+			  },
+        
+              {
+				"Film":3,
+				"Title":"Harry Potter and the Prisoner of Azkaban",
+				"Year":2004,
+				"Director":"Alfonso Cuarón",
+				"Producers":"David Heyman, Chris Columbus and Mark Radcliffe",
+				"Screenwriter": "Steve Kloves",
+				"Composer":"John Williams",
+				"Budget":"$130,000,000.00",
+				"BoxOffice": "$796,688,549",
+				"Image":"hp3.jpg"
+			  },
+			  {
+				"Film":4,
+				"Title":"Harry Potter and the Goblet of Fire",
+				"Year":2005,
+				"Director":"Mike Newell",
+				"Producer":"David Heyman",
+				"Screenwriters":"Steve Kloves",
+				"Composer": "Patrick Doyle",
+				"Budget":"$150,000,000.00",
+				"BoxOffice":"$896,911,078",
+				"Image":"hp4.jpg"
+			  },
+			  {
+				"Film":5,
+				"Title":"Harry Potter and the Order of the Phoenix",
+				"Year":2007,
+				"Director":"David Yates",
+				"Producers":"David Heyman and David Barron",
+				"Screenwriter": "Michael Goldenberg",
+				"Composer":"Nicholas Hooper",
+				"Budget":"$150,000,000.00",
+				"BoxOffice":"$942,885,929",
+				"Image":"hp5.jpg"
+			  },
+			  {
+				"Film":6,
+				"Title":"Harry Potter and the Half-Blood Price",
+				"Year":2009,
+				"Director":"David Yates",
+				"Producers":"David Heyman and David Barron",
+				"Screenwriters":"Steve Kloves",
+				"Composer":"Nicholas Hooper",
+				"Budget":"$250,000,000.00",
+				"BoxOffice":"$934,416,487",
+				"Image":"h6.jpg"
+			  },
+			  {
+				"Film":7,
+				"Title":"Harry Potter and the Deathly Hallows - Part 1",
+				"Year":2010,
+				"Director":"David Yates",
+				"Producers":"David Heyman, David Barron and J. K. Rowling",
+				"Screenwriters":"Steve Kloves",
+				"Composer":"David Arnold",
+				"Budget":"$250,000,000.00",
+				"BoxOffice":"$976,283,305",
+				"Image":"hp7.jpg"
+			  },
+			  {
+				"Film":8,
+				"Title":"Harry Potter and the Deathly Hallows - Part 2",
+				"Year":2011,
+				"Director":"David Yates",
+				"Producers":"David Heyman, David Barron and J. K. Rowling",
+				"Screenwriters":"Steve Kloves",
+				"Composer":"Alexandre Desplat",
+				"Budget":"$250,000,000.00",
+				"BoxOffice":"$1,341,511,219",
+				"Image":"hp8.jpg"
+			  }
+
+	]
+}
